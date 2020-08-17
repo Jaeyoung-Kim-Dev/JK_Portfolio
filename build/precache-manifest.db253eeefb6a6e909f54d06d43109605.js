eb5fbea3cb3e97a4d0bd58336764c7a7ac629a5f
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2020-08-17 02:31:41 -0600

    Add build folder

---
+++ build/precache-manifest.db253eeefb6a6e909f54d06d43109605.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76eda90ae4d1868263d4a8cbb2390df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "876218ce82a9c2a5ad5d",
+    "url": "/static/css/2.b9b24cdd.chunk.css"
+  },
+  {
+    "revision": "6f3208ed6f237ea59524",
+    "url": "/static/css/main.2f8f6787.chunk.css"
+  },
+  {
+    "revision": "876218ce82a9c2a5ad5d",
+    "url": "/static/js/2.333a081f.chunk.js"
+  },
+  {
+    "revision": "4d833975c154ea21126df2a4e5867037",
+    "url": "/static/js/2.333a081f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f3208ed6f237ea59524",
+    "url": "/static/js/main.c53f6b9e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b76f264c447b10189167acb4bf0addf5",
+    "url": "/static/media/Resume(Jaeyoung_Kim).b76f264c.pdf"
+  },
+  {
+    "revision": "0589ea93433b4850338f4ba8fd10ed2c",
+    "url": "/static/media/amazonwebservices-original-wordmark.0589ea93.svg"
+  },
+  {
+    "revision": "24abeee74d2a27988ad94666d117d356",
+    "url": "/static/media/angularjs-original-wordmark.24abeee7.svg"
+  },
+  {
+    "revision": "76d7c6aabe5de63af067918dc270986c",
+    "url": "/static/media/bootstrap-plain-wordmark.76d7c6aa.svg"
+  },
+  {
+    "revision": "0d87bce7183cf43bcc47e59b644bb5ce",
+    "url": "/static/media/c-line.0d87bce7.svg"
+  },
+  {
+    "revision": "6517350787fd946857b0469332b9bae3",
+    "url": "/static/media/chatwithtitanic.65173507.jpg"
+  },
+  {
+    "revision": "16f0bb70fa9ae183d79a19270918a518",
+    "url": "/static/media/computer-pc-svgrepo-com.16f0bb70.svg"
+  },
+  {
+    "revision": "fff793948eaad4a21b6fb5712888d62d",
+    "url": "/static/media/github-original-wordmark.fff79394.svg"
+  },
+  {
+    "revision": "a8b24b933dec5eb71ff512e302fafe78",
+    "url": "/static/media/icons8-powershell.a8b24b93.svg"
+  },
+  {
+    "revision": "e6d87e2dca08c953fe3acd1275ed4f4e",
+    "url": "/static/media/java-original-wordmark.e6d87e2d.svg"
+  },
+  {
+    "revision": "97b926d535d613c268749b16a22606ef",
+    "url": "/static/media/jksportsclub.97b926d5.jpg"
+  },
+  {
+    "revision": "2bf747f09a4f9135fbd3032432f4158b",
+    "url": "/static/media/linux-original.2bf747f0.svg"
+  },
+  {
+    "revision": "e155dcb58cf72028d2897e1182e7332f",
+    "url": "/static/media/me.e155dcb5.jpg"
+  },
+  {
+    "revision": "b1847af9e7cf00cbe2d44e9fa173d281",
+    "url": "/static/media/mongodb-original-wordmark.b1847af9.svg"
+  },
+  {
+    "revision": "f13b681bf07a1d23ee3fd1c591385ce9",
+    "url": "/static/media/nodejs-original-wordmark.f13b681b.svg"
+  },
+  {
+    "revision": "369bae91ff659183c379c49cc099660b",
+    "url": "/static/media/oracle-original.369bae91.svg"
+  },
+  {
+    "revision": "72bbce5b3b0d6ad88c2491a4cb82a6de",
+    "url": "/static/media/python-original-wordmark.72bbce5b.svg"
+  },
+  {
+    "revision": "fadd9c9566ccc46c6b0e73caca5860de",
+    "url": "/static/media/react-original-wordmark.fadd9c95.svg"
+  }
+]);
